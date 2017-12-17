592c5d7ed71798aabd3151054cbaeecd71a78db9
Jesper Brynildsen <jesper_bry@hotmail.com>
2017-12-17 23:00:19 +0100

    Delete setup.py

--- algo/setup.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from setuptools import setup
-
-setup(
-    name='algo',    # This is the name of your PyPI-package.
-    version='1.2',                          # Update the version number for new releases
-    scripts=['algo']                  # The name of your scipt, and also the command you'll be using for calling it
-)
