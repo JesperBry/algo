1b15fe590b4eabb33bf55243d0b9881c59fe2674
Jesper Brynildsen <jesper_bry@hotmail.com>
2017-12-17 23:01:07 +0100

    added base files

---
+++ algo/setup.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+
+from distutils.core import setup
+
+setup(
+  name = 'algo',
+  packages = ['algo'], # this must be the same as the name above
+  version = '1.2.0',
+  description = 'A package with the most common sorting algorithms. All algorithms are based on the book; Introduction to algorithms by Cormen.',
+  author = 'JesperBry',
+  author_email = 'jespebr@stud.ntnu.no',
+  url = 'https://github.com/JesperBry', # use the URL to the github repo
+  download_url = 'https://jesperbry.github.io/', # I'll explain this in a second
+  keywords = ['algorithms', 'Cormen', 'sorting', 'algo', 'Python'], # arbitrary keywords
+  classifiers = [],
+)
