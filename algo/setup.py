faa5a4699fecf5e32b1822789300cf9454e22682
Jesper Brynildsen <jesper_bry@hotmail.com>
2017-12-17 21:46:08 +0100

    added algo.py dist

---
+++ algo/setup.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+
+from setuptools import setup
+
+setup(
+    name='algo',    # This is the name of your PyPI-package.
+    version='1.2',                          # Update the version number for new releases
+    scripts=['algo']                  # The name of your scipt, and also the command you'll be using for calling it
+)
