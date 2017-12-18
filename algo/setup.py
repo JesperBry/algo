9f6bb6e0de17e3bff995f6c881bb5faed78fe3a0
Jesper Brynildsen <jesper_bry@hotmail.com>
2017-12-18 18:45:24 +0100

    Update for 1.2.2

--- algo/setup.py
+++ algo/setup.py
@@ -5,12 +5,12 @@ from distutils.core import setup
 setup(
   name = 'algo',
   packages = ['algo'], # this must be the same as the name above
-  version = '1.2.0',
+  version = '1.2.2',
   description = 'A package with the most common sorting algorithms. All algorithms are based on the book; Introduction to algorithms by Cormen.',
   author = 'JesperBry',
   author_email = '#',
-  url = 'https://github.com/JesperBry', # use the URL to the github repo
-  download_url = 'https://jesperbry.github.io/',
+  url = 'https://github.com/JesperBry/algo', # URL to the github repo
+  download_url = 'https://jesperbry.github.io/algo/',
   keywords = ['algorithms', 'Cormen', 'sorting', 'algo', 'Python'], # arbitrary keywords
   classifiers = [],
 )
