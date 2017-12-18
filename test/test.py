36fd3e1d5b94e9ccc8f29b85a50e0f0bfbe9c53b
Jesper Brynildsen <jesper_bry@hotmail.com>
2017-12-18 18:53:59 +0100

    added test.py

---
+++ test/test.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+
+import algo.algo
+
+algo.algo.help()
+
+A = [27, 4, 15, 9, 110, 0, 13, 25, 1, 17, 802, 66, 25, 45, 97, 9]
+
+algo.algo.mergeSort(A, 0, len(A)-1)
+print(A)
